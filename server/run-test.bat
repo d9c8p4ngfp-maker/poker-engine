7597288262345c506a1da22cd2af51fe5f806d4f
poker-dev <dev@poker.local>
2026-06-20 22:47:13 +0800

    chore: remove non-source files (docs, scripts, IDE config)

--- server/run-test.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set JAVA_HOME=C:\Users\Admin\.tools\jdk-21
-cd /d D:\poker-web\server
-call mvnw.cmd test
--- server/start-backend.bat
+++
@@ -1,3 +0,0 @@
-set JAVA_HOME=C:\Users\Admin\.tools\jdk-21
-cd /d D:\poker-web\server
-call mvnw.cmd spring-boot:run
